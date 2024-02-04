8efa84d1be3ec0212e0ed65a0fca0ade667dae88
jldroid <admin@sandboxcsp.com>
2024-02-04 02:13:57 -0500

    added application host(s)

--- ansible/scripts/ansible_host_ssh.sh
+++ ansible/scripts/ansible_host_ssh.sh
@@ -1,5 +1,6 @@
 
-ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.15
-ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.37
-ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.187
-ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.211
+# ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.15
+# ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.37
+ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.38
+# ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.187
+# ssh-copy-id -i ~/.ssh/id_ed25519.pub root@192.168.0.211
